8c2ff599c6afb196dddca3d4bc477ac78c95992e
xzx482 <xzx482@qq.com>
2022-03-05 14:14:17 +0800

    1

--- captchaGenerator.py
+++ captchaGenerator.py
@@ -21,15 +21,16 @@ def gen_captcha_text_and_image():
     return captcha_text, captcha_image
 
 if __name__ == '__main__':
-    count = 1000
+    count = 10000
     path = settings.TRAIN_DATASET_PATH    #通过改变此处目录，以生成 训练、测试和预测用的验证码集
     if not os.path.exists(path):
         os.makedirs(path)
         
-    for i in range(count):
-        now = str(int(time.time()))
-        text, image = gen_captcha_text_and_image()
-        filename = text+'_'+now+'.jpg'
-        image.save(path  + os.path.sep +  filename)
+    for i in range(count//100):
+        for j in range(100):
+            now = str(int(time.time()))
+            text, image = gen_captcha_text_and_image()
+            filename = text+'_'+now+'.jpg'
+            image.save(path  + os.path.sep +  filename)
         print('saved %d : %s' % (i+1,filename))
 
